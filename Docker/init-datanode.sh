5f97d320a54603bf090742fd45e42bf32dfdfe35
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-04-09 11:36:12 +0100

    hadoop working successfully

--- Docker/init-datanode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p /hadoop/dfs/data
-chown -R root:root /hadoop/dfs/data  # or whatever user is running it
-chmod 700 /hadoop/dfs/data
-
-rm -rf /opt/hadoop/data/dataNode/*
-chown -R hadoop:hadoop /opt/hadoop/data/dataNode
-chmod 755 /opt/hadoop/data/dataNode
-hdfs datanode
--- Docker/start-hdfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p /hadoop/dfs/name
-if [ ! -d "/opt/hadoop/data/nameNode/current" ]; then
-    echo "Formatting NameNode..."
-    hdfs namenode -format
-fi
-hdfs namenode
