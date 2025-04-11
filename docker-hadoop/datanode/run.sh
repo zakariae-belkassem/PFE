8e389ee3785565f02429816df409b9d8a2421330
Zakariaae Belkassem <zakariaebelkassem@gmail.com>
2025-04-11 12:37:57 +0100

    linking everything together , need to add impala later

--- docker-hadoop/datanode/run.sh
+++ docker-hadoop/datanode/run.sh
@@ -5,5 +5,5 @@ if [ ! -d $datadir ]; then
   echo "Datanode data directory not found: $datadir"
   exit 2
 fi
-
+hdfs dfs -put ./data.json /user/data.json
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
